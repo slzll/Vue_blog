b42e989bb5829faff1b273a14db9a9682728a43b
slzll <1259654637@qq.com>
2019-05-31 17:13:56 +0800

    修改单元测试用例

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,13 +1,19 @@
-import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { expect } from 'chai';
+import { shallowMount } from '@vue/test-utils';
+import ArticleItem from '@/components/ArticleItem.vue';
 
-describe('HelloWorld.vue', () => {
+describe('ArticleItem.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).to.include(msg)
-  })
-})
+    const itemData = {
+      title: 'title',
+      created_date: new Date().toString(),
+      tags: []
+    };
+    const wrapper = shallowMount(ArticleItem, {
+      propsData: { itemData }
+    });
+    expect(wrapper.text())
+      .to
+      .include(itemData.title);
+  });
+});
