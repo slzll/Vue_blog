a296cb952139cf602ab3d4b54ff6d15b5c2a5fed
slzll <1259654637@qq.com>
2019-07-12 08:33:57 +0800

    2019-07-12 commit

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ Vue.use(VueRouter);
 
 // 导出路由 在 main.js 里使用
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   routes
 });
 
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,26 @@
 module.exports = {
+  pwa: {
+    name: 'vue-blog',
+    themeColor: '#6476DB',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    /**
+     * 两个模式，GenerateSW（默认）和 InjectManifest
+     * GenerateSW 在我们build项目时候，每次都会新建一个service worker文件
+     * InjectManifest 可以让我们编辑一个自定义的service worker文件，实现更多的功能，并且可以
+     * 拿到预缓存列表
+     */
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'src/service-worker.js'
+      // ...other Workbox options...
+    }
+  },
   devServer: {
-    proxy: 'https://www.slzll.com'
+    proxy: 'https://www.slzll.com',
+    port: 8088
   },
   chainWebpack: config => {
     // vue添加iview-loader，可以通过vue inspect --rule vue查看vue的loader
