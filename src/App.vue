f52c90db6257bc61d21f5abfb62030dd4274b20e
slzll <1259654637@qq.com>
2019-07-27 22:26:38 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -4,57 +4,59 @@
     <!--<custom-cursor/>-->
     <Layout>
       <Header class="app-header">
-        <blog-header/>
+        <blog-header />
       </Header>
       <Content class="app-view">
-        <router-view/>
+        <keep-alive>
+          <router-view />
+        </keep-alive>
       </Content>
       <Footer>
-        <blog-footer/>
+        <blog-footer />
       </Footer>
     </Layout>
   </div>
 </template>
 <script>
-  import BlogHeader from '@/components/Header';
-  import BlogFooter from '@/components/Footer';
+import BlogHeader from "@/components/Header";
+import BlogFooter from "@/components/Footer";
 
-  export default {
-    components: {
-      BlogFooter,
-      BlogHeader
-    }
-  };
+export default {
+  components: {
+    BlogFooter,
+    BlogHeader
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-  @import "./styles/mixin";
+@import "./styles/mixin";
 
-  #app {
-    /*cursor: none !important;*/
+#app {
+  /*cursor: none !important;*/
 
-    .ambient-canvas-bg {
-      position: relative;
-      z-index: 1;
-    }
-
-    .app-header {
-      position: relative;
-      z-index: 2;
-      box-shadow: 0 0 10px 0 #333;
-    }
-
-    .app-view {
-      position: relative;
-      z-index: 2;
-    }
-
-    .ivu-layout-header {
-      background-image: linear-gradient(135deg, #3B2667 10%, #BC78EC 100%);
-    }
-
-    .ivu-layout-footer {
-      padding: 0;
-    }
+  .ambient-canvas-bg {
+    position: relative;
+    z-index: 1;
   }
+
+  .app-header {
+    position: relative;
+    z-index: 2;
+    box-shadow: 0 0 10px 0 #333;
+  }
+
+  .app-view {
+    position: relative;
+    z-index: 2;
+  }
+
+  .ivu-layout-header {
+    background-image: linear-gradient(135deg, #3b2667 10%, #bc78ec 100%);
+  }
+
+  .ivu-layout-footer {
+    padding: 0;
+  }
+}
 </style>
