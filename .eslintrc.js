eb3b148ca225352f13687f30cd563ceac0505bd4
slzll <1259654637@qq.com>
2019-07-28 17:13:25 +0800

    commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,24 +3,21 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ["plugin:vue/essential", "@vue/standard"],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'comma-dangle': 'off',
-    'indent': 'off',
-    'no-tabs': 'off',
-    'no-mixed-spaces-and-tabs': 'off',
-    'quotes': 'off',
-    'semi': 'off',
-    'space-before-function-paren': 'off',
-    'one-var': 'off',
-    'vue/no-parsing-error': 'off'
+    "no-console": "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "comma-dangle": "off",
+    indent: "off",
+    "no-tabs": "off",
+    "no-mixed-spaces-and-tabs": "off",
+    quotes: "off",
+    semi: "off",
+    "space-before-function-paren": "off",
+    "one-var": "off",
+    "vue/no-parsing-error": "off"
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   }
 };
